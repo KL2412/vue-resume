a6030faa3fa33679c26fbf48c3f4b7afff1b53d8
khailoong-nw <khailoong@newswav.com>
2024-07-04 10:52:07 +0800

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,14 @@
 <script setup>
 import { ref } from 'vue';
 
-const name = ref('Khai Loong, Lim (KayL)');
+const name = ref('Khai Loong, Lim');
 const title = ref('Data Science Student - Junior Full Stack Developer');
 const description = ref('21 Years Old, Kuala Lumpur - Malaysia');
 </script>
 
 <template>
-  <div class=" pt-12 intro route bg-cover bg-fixed bg-center d-print-none" style="background-image: url(img/cat.jpg)">
-    <div class="flex items-center justify-center h-full">
+  <div class="pt-12 intro">
+    <div class="flex items-center justify-center h-full bg-black bg-opacity-60">
       <div class="container text-center">
         <h1 class="text-white font-semibold text-7xl mb-4">I'm {{ name }}</h1>
         <h2 class="text-blue-600 text-4xl">{{ title }}</h2>
